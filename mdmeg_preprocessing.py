d02885b11c1c9d86675b1a8254412feea8eff79c
Vince Polito <vince.polito@mq.edu.au>
2024-08-01 14:22:54 +1000

    initial commit of new mdmgeg script

---
+++ mdmeg_preprocessing.py
@@ -0,0 +1,253 @@
+'''
+17.1.1.	Preprocessing steps based on MNE-BIDS-Pipeline
+17.1.1.1.	Data de-noising using TSPCA-based noise reduction based on MEG reference sensors
+17.1.1.2.	Downsample to 250Hz
+17.1.1.3.	Apply low- and high-pass filters:
+17.1.1.3.1.	For visual longterm potentiation and auditory oddball tasks: 0.1-40Hz
+17.1.1.3.2.	For resting state: .1-150Hz with 50, 100 and 150 notch filters.
+17.1.1.4.	Extract epochs
+17.1.1.5.	Automated noisy channel identification and interpolation using RANSAC
+17.1.1.6.	Automated artifact noise removal by ICA based on correlations of ICA components with MEG sensors EOG artifacts and ECG related components using cross-trial phase statistic
+
+'''
+
+import os
+import mne
+import meegkit # for TSPCA
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+
+from mne.preprocessing import find_bad_channels_maxwell
+#from autoreject import get_rejection_threshold  # noqa
+#from autoreject import Ransac  # noqa
+#from autoreject.utils import interpolate_bads  # noqa
+from mne.decoding import EMS
+
+import my_preprocessing
+
+# We can use the `decim` parameter to only take every nth time slice.
+# This speeds up the computation time. Note however that for low sampling
+# rates and high decimation parameters, you might not detect "peaky artifacts"
+# (with a fast timecourse) in your data. A low amount of decimation however is
+# almost always beneficial at no decrease of accuracy.
+
+#os.chdir("/Users/mq20096022/Downloads/MD_pilot1/")
+#os.chdir("/Users/mq20096022/Downloads/220112_p003/")
+
+# set up file and folder paths here
+#exp_dir = "/mnt/d/Work/analysis_ME197/"
+exp_dir = "C:/sync/OneDrive - Macquarie University/Studies/19_MEG_Microdosing/analysis/meg/"
+subject_MEG = '220503_87225_S1' #'230426_72956_S2' #'220112_p003'
+meg_task = '_oddball' #'_oddball' #''
+
+# the paths below should be automatic
+#data_dir = exp_dir + "data/"
+data_dir = "C:/sync/OneDrive - Macquarie University/Studies/19_MEG_Microdosing/data/ACQUISITION/"
+processing_dir = exp_dir + "processing/"
+results_dir = exp_dir + "results/"
+#meg_dir = data_dir + subject_MEG + "/meg/"
+meg_dir = data_dir + subject_MEG + "/"
+save_dir = processing_dir + subject_MEG + "/"
+figures_dir_meg = results_dir + 'oddball' + '/Figures/' # where to save the figures for all subjects
+epochs_fname = save_dir + subject_MEG + meg_task + "-epo.fif"
+ica_fname = save_dir + subject_MEG + meg_task + "-ica.fif"
+#os.system('mkdir -p ' + save_dir) # create the folder if needed
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+if not os.path.exists(figures_dir_meg):
+    os.makedirs(figures_dir_meg)
+print("exp_dir",exp_dir)
+print("data_dir",data_dir)
+print("processing_dir",processing_dir)
+print("results_dir",results_dir)
+print("meg_dir",meg_dir)
+print("save_dir",save_dir)
+print("figures_dir_meg",figures_dir_meg)
+print("epochs_fname",epochs_fname)
+print("ica_fname",ica_fname)
+
+
+#%% === Read raw data === #
+
+#print(glob.glob("*_oddball.con"))
+fname_raw = glob.glob(meg_dir + "*" + meg_task + ".con")
+fname_elp = glob.glob(meg_dir + "*.elp")
+fname_hsp = glob.glob(meg_dir + "*.hsp")
+fname_mrk = glob.glob(meg_dir + "*.mrk")
+
+# Raw extraction ch misc 23-29 = triggers
+# ch misc 007 = audio
+raw = mne.io.read_raw_kit(
+    fname_raw[0],  # change depending on file i want
+    mrk=fname_mrk[0],
+    elp=fname_elp[0],
+    hsp=fname_hsp[0],
+    stim=[*[166], *range(182, 190)],
+    slope="+",
+    stim_code="channel",
+    stimthresh=2,  # 2 for adult (1 for child??)
+    preload=True,
+    allow_unknown_format=False,
+    verbose=True,
+)
+raw.crop(tmax=120)
+# Apply TSPCA for noise reduction
+print("Starting TSPCA")
+noisy_data = raw.get_data(picks="meg").transpose()
+noisy_ref = raw.get_data(picks=[160,161,162]).transpose()
+data_after_tspca, idx = meegkit.tspca.tsr(noisy_data, noisy_ref)[0:2]
+raw._data[0:160] = data_after_tspca.transpose()
+print("Finished TSPCA")
+
+print("Starting resampling")
+raw.resample(250)
+print("Resampled")
+
+print("Starting filter")
+raw.filter(l_freq=0.1, h_freq=40)
+print("Finished filter")
+
+
+print("Finding events")
+# Finding events
+events = mne.find_events(
+    raw,
+    output="onset",
+    consecutive=False,
+    min_duration=0,
+    shortest_event=1,  # 5 for adult
+    mask=None,
+    uint_cast=False,
+    mask_type="and",
+    initial_event=False,
+    verbose=None,
+)
+
+# get rid of audio triggers for now
+events = np.delete(events, np.where(events[:, 2] == 166), 0)
+
+# re-code standard & deviant trials as '1' and '2'
+#events = copy.deepcopy(events)
+std_dev_bool = np.insert(np.diff(events[:, 2]) != 0, 0, "True") # find all deviants & mark as "True"
+for idx, event in enumerate(std_dev_bool):
+    if event and idx > 0: # for all deviants (except for the very first trial, which we won't use)
+        events[idx, 2] = 2 # code current trial as '2'
+        if events[idx - 1, 2] != 2:
+            events[idx - 1, 2] = 1 # code previous trial as '1'
+
+# specify the event IDS (these will be used during epoching)
+event_ids = {
+    "standard": 1,
+    "deviant": 2,
+}
+
+
+# Adjust trigger timing based on audio channel signal 
+
+# get raw audio signal from ch166
+aud_ch_data_raw = raw.get_data(picks="MISC 007")
+
+def getEnvelope(inputSignal):
+    # Taking the absolute value
+    absoluteSignal = []
+    for sample in inputSignal:
+        absoluteSignal.append(abs(sample))
+    absoluteSignal = absoluteSignal[0]
+
+    # Peak detection
+    intervalLength = 5  # Experiment with this number!
+    outputSignal = []
+
+    # Like a sample and hold filter
+    for baseIndex in range(intervalLength, len(absoluteSignal)):
+        maximum = 0
+        for lookbackIndex in range(intervalLength):
+            maximum = max(absoluteSignal[baseIndex - lookbackIndex], maximum)
+        outputSignal.append(maximum)
+
+    outputSignal = np.concatenate(
+        (
+            np.zeros(intervalLength),
+            np.array(outputSignal)[:-intervalLength],
+            np.zeros(intervalLength),
+        )
+    )
+    # finally binarise the output at a threshold of 2.5  <-  adjust this 
+    # threshold based on diagnostic plot below!
+    return np.array([1 if np.abs(x) > 0.2 else 0 for x in outputSignal])
+
+#raw.load_data().apply_function(getEnvelope, picks="MISC 006")
+envelope = getEnvelope(aud_ch_data_raw)
+envelope = envelope.tolist() # convert ndarray to list
+# detect the beginning of each envelope (set the rest of the envelope to 0)
+new_stim_ch = np.clip(np.diff(envelope),0,1)
+# find all the 1s (i.e. audio triggers)
+stim_tps = np.where(new_stim_ch==1)[0]
+
+# compare number of events from trigger channels & from AD
+print("Number of events from trigger channels:", events.shape[0])
+print("Number of events from audio channel (166) signal:", stim_tps.shape[0])
+
+# plot any problematic time period to aid diagnosis
+'''
+test_time = 454368
+span = 10000
+plt.figure()
+plt.plot(aud_ch_data_raw[0], 'b')
+#plt.plot(outputSignal, 'r')
+for i in range(events.shape[0]):
+   plt.axvline(events[i,0], color='b', lw=2, ls='--')
+   plt.axvline(stim_tps[i], color='r', lw=2, ls='--')
+plt.xlim(test_time-span, test_time+span)
+plt.show()
+'''
+
+# apply timing correction onto the events array
+events_corrected = copy.copy(events) # work on a copy so we don't affect the original
+
+# Missing AD triggers can be handled:
+# if there's an AD trigger between 100-200ms after normal trigger (this ensures 
+# we've got the correct trial), update to AD timing;
+# if there's no AD trigger in this time range, discard the trial
+AD_delta = []
+missing = [] # keep track of the trials to discard (due to missing AD trigger)
+for i in range(events.shape[0]):
+    idx = np.where((stim_tps > events[i,0]) & (stim_tps <= events[i,0]+200))
+    if len(idx[0]): # if an AD trigger exists within 200ms of trigger channel
+        idx = idx[0][0] # use the first AD trigger (if there are multiple)
+        AD_delta.append(stim_tps[idx] - events[i,0]) # keep track of audio delay values (for histogram)
+        events_corrected[i,0] = stim_tps[idx] # update event timing
+    else:
+        missing.append(i)
+# discard events which could not be corrected
+events_corrected = np.delete(events_corrected, missing, 0)
+print("Could not correct", len(missing), "events - these were discarded!")
+
+# histogram showing the distribution of audio delays
+n, bins, patches = plt.hist(
+    x=AD_delta, bins="auto", color="#0504aa", alpha=0.7, rwidth=0.85
+)
+plt.grid(axis="y", alpha=0.75)
+plt.xlabel("Delay (ms)")
+plt.ylabel("Frequency")
+plt.title("Audio Detector Delays")
+plt.text(
+    70,
+    50,
+    r"$mean="
+    + str(round(np.mean(AD_delta)))
+    + ", std="
+    + str(round(np.std(AD_delta)))
+    + "$",
+)
+maxfreq = n.max()
+# set a clean upper y-axis limit
+plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+plt.show()
+
+
+X = range(10)
+plt.plot(X, [x*x for x in X])
+plt.show()
